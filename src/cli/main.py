ba22008b5e9795c9281bb3850f8241cdd6636778
Santi G <24705975+santiagoa58@users.noreply.github.com>
2023-09-09 19:43:38 -0400

    pretty print more manwha details

--- src/cli/main.py
+++ src/cli/main.py
@@ -1,10 +1,28 @@
+from pandas import DataFrame
 from src.recommender.manwha_recommender import (
     load_manwha_recommender_model,
-    MANWHA_NOT_FOUND,
 )
-
+from src.utils.constants import MANWHA_NOT_FOUND, UNKNOWN
 
 RECOMENDER = load_manwha_recommender_model()
+
+
+def print_manwha_info(manwha_name_df: DataFrame):
+    # sort by rating
+    manwha_name_df = manwha_name_df.sort_values(by="rating", ascending=False)
+    for _, row in manwha_name_df.iterrows():
+        alt_name = row["altName"]
+        name = (
+            f'{row["name"]} (AKA: {alt_name})'
+            if alt_name and alt_name != UNKNOWN
+            else row["name"]
+        )
+        print(f"{name}")
+        print(f"\tRating: {row['rating']}")
+        print(f"\tTags: {row['tags']}")
+        print(f"\tYears Active: {row['years']}")
+        print(f"\tChapters: {row['chapters']}")
+        print("-" * 50)  # Just a separator for readability
 
 
 def main():
@@ -20,15 +38,13 @@ def main():
 
         # Get recommendations
         results = RECOMENDER.recommend(manhwa_name)
-
         # Display recommendations
         if results == MANWHA_NOT_FOUND:
             print(results)
         else:
             recommendations, target_manwha_name = results
-            print(f"\nRecommended Manwhas for {target_manwha_name}:")
-            for idx, rec in enumerate(recommendations, 1):
-                print(f"{idx}. {rec}")
+            print(f"\nRecommended Manwhas for {target_manwha_name}:\n")
+            print_manwha_info(recommendations)
         print("\n")
 
 
